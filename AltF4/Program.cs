acfd7608e55ee4566c2a456593db2419c8e270ae
Markellus <gmx@marcel-bulla.de>
2017-06-10 16:42:00 +0200

    Initial commit, v1.0.1

---
+++ AltF4/Program.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Windows.Automation;
+using System.Collections.Generic;
+using System.IO;
+
+namespace AltF4
+{
+    class Program
+    {
+        private const int WH_KEYBOARD_LL = 13;
+        private const int WM_KEYDOWN = 0x0100;
+        private const int WM_SYSKEYDOWN = 0x0104;
+        private const int WM_KEYUP = 0x0101;
+        private const int WM_SYSKEYUP = 0x0105;
+        private static LowLevelKeyboardProc _proc = HookCallback;
+        private static IntPtr _hookID = IntPtr.Zero;
+
+        internal delegate IntPtr LowLevelKeyboardProc(int nCode, IntPtr wParam, IntPtr lParam);
+
+        private static List<Keys> listKeys = new List<Keys>();
+
+        private static bool bKilled = false;
+        private static Process procFocus;
+
+        private static void Main(string[] args)
+        {
+            AutomationFocusChangedEventHandler focusHandler = OnFocusChanged;
+            Automation.AddAutomationFocusChangedEventHandler(focusHandler);
+
+            _hookID = SetHook(_proc);
+
+            Application.Run();
+
+            UnhookWindowsHookEx(_hookID);
+        }
+
+        private static IntPtr HookCallback(int nCode, IntPtr wParam, IntPtr lParam)
+        {
+            if (nCode >= 0)
+            {
+                int vkCode = Marshal.ReadInt32(lParam);
+                Keys code = (Keys)vkCode;
+
+                if (wParam == (IntPtr)WM_KEYDOWN || wParam == (IntPtr)WM_SYSKEYDOWN)
+                {
+                    
+
+                    if (!listKeys.Contains(code))
+                    {
+                        listKeys.Add(code);
+                    }
+
+                    if (listKeys.Contains(Keys.F4) &&
+                       (listKeys.Contains(Keys.LMenu) || listKeys.Contains(Keys.Alt)))
+                    {
+                        if (!bKilled && procFocus != null && !procFocus.HasExited)
+                        {
+                            procFocus.Kill();
+                            bKilled = true;
+                        }
+                        return (IntPtr)1;
+                    }
+                }
+                else if (wParam == (IntPtr)WM_KEYUP || wParam == (IntPtr)WM_SYSKEYUP)
+                {
+                    if (listKeys.Contains(code))
+                    {
+                        listKeys.Remove(code);
+                    }
+
+                    if (!listKeys.Contains(Keys.F4) &&
+                        !listKeys.Contains(Keys.LMenu) &&
+                        !listKeys.Contains(Keys.Alt))
+                    {
+                        bKilled = false;
+                    }
+                }
+            }
+
+            return CallNextHookEx(_hookID, nCode, wParam, lParam);
+        }
+
+        private static IntPtr SetHook(LowLevelKeyboardProc proc)
+        {
+            using (Process curProcess = Process.GetCurrentProcess())
+            using (ProcessModule curModule = curProcess.MainModule)
+            {
+                return SetWindowsHookEx(WH_KEYBOARD_LL, proc,
+                    GetModuleHandle(curModule.ModuleName), 0);
+            }
+        }
+
+        private static void OnFocusChanged(object sender, AutomationFocusChangedEventArgs e)
+        {
+            AutomationElement focusedElement = sender as AutomationElement;
+            if (focusedElement != null)
+            {
+                int processId = focusedElement.Current.ProcessId;
+                procFocus = Process.GetProcessById(processId);
+            }
+        }
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr SetWindowsHookEx(int idHook, LowLevelKeyboardProc lpfn, IntPtr hMod, uint dwThreadId);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool UnhookWindowsHookEx(IntPtr hhk);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr CallNextHookEx(IntPtr hhk, int nCode, IntPtr wParam, IntPtr lParam);
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr GetModuleHandle(string lpModuleName);
+    }
+}
